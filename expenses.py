b4a94f7e7fbd4c1f90b2d3b421e999991aabe76c
Me <ecud@ECUDs-MacBook-Pro.local>
2024-02-15 21:06:37 -0500

    First commit

---
+++ expenses.py
@@ -0,0 +1,69 @@
+import csv
+def main():
+
+    #Get user input
+    get_income()
+    get_expenses()
+
+    #Calculate
+    #calculate()
+
+    #Output
+    #print("Your budget is complete")
+
+#Get user input
+def get_income():
+        #-Net pay(amount after taxes) what is net pay?
+    net_income = float(input("What is your montly net income? ")
+)
+        #-Last 2 pay date and amount
+    print("Enter your last two pay dates ")
+        #-most recent pay date
+    recent_pay_date = input("Enter your most recent pay date (MM-DD-YYYY): ")
+        #-next pay date
+    recent_pay_date = input("Enter your last pay date before the most recent one (MM-DD-YYYY): ")
+
+def get_expenses():
+
+    #Collect expenses
+    #Categories needed
+    #- [ ] Name
+    #- [ ] Amount
+    #- [ ] Due Date
+    print("Enter your expenses. Include the Name, Amount, and Due Date (1-30) for each expense.")
+    print("Example: Rent, 1000, 1")
+
+    expenses = [{}]
+
+    while True:
+        expenses = input("Enter an expense (or type 'done' to finish) ").strip().capitalize()
+        if expenses == "done":
+            break
+        else:
+            expenses.append(expenses)
+    
+    Expense_name, Amount, Due_Date = expenses.split(", ")
+
+    with open('expenses.csv' "a") as file:
+        writer = csv.Dictwriter(file, fieldnames=["Expense","Amount", "Due Date"])
+        writer.writerow({"Expense": Expense_name, "Amount": Amount, "Due Date": Due_Date})
+    
+
+
+
+#Output
+    #Categories 
+    #Income - Expenses
+    #CSP
+    #-Fixed cost 50%
+    #-Savings 20%
+    #-Wants 30%
+
+#Bi-weekly checks
+    #-first check amount
+    #—total expenses coming out of that check
+    #-last check amount
+    #—total expenses coming out of that check
+
+if __name__ == "__main__":
+    main()
