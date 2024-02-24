342680f4ba908c92925e73bb64d3585aab9e39d3
Me <davidhyppolitedh@gmail.com>
2024-02-23 22:21:04 -0500

    Small updates

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel
-from main import engine
+from . import models, database
 from routes.expenses import Expense
 from routes.income import Income
+
 
 
 app = FastAPI()
 
 
-def create_db_and_tables(): 
-    SQLModel.metadata.create_all(engine)
+def on_startup():
+    database.create_db_and_tables
 
 
 
