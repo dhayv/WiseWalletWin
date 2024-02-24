f1775488aafe1b120347b8bc482bdff444a46b62
Me <davidhyppolitedh@gmail.com>
2024-02-24 18:04:52 -0500

    Add lifespan event server finally starts properly adressed errors

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel
 
 
 sqlite_file_name = "database.db"
@@ -7,3 +7,5 @@ sqlite_url = f"sqlite:////{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
 
+def create_db_and_tables(): 
+    SQLModel.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI
-from fastapi.events import Lifespan
+from contextlib import asynccontextmanager
 from router.expenses import router as expense_router
 from router.income import router as income_router
 import database
 
+print("line before api")
 app = FastAPI()
 
-@app.lifespan.on_startup
-async def startup():
+print("line after api")
+@asynccontextmanager
+async def lifespan(app:FastAPI):
     await database.create_db_and_tables()
     print("Database Created")
+    #try:
+        #yield
+    #finally:
+        # Cleanup code here, if necessary
+        #print("Application shutdown")
 
-app.include_router(income_router)
-app.include_router(expense_router)
+@app.router.lifespan_context
+async def lifespan(app):
+    async with lifespan():
+        yield
+
+
+
+
+app.include_router(expense_router, prefix="/expenses")
+app.include_router(income_router, prefix="/income")
+
