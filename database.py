7b027a1974df2bd23de5558e8aa8443aa0ba8b87
Me <davidhyppolitedh@gmail.com>
2024-02-24 19:17:53 -0500

    lifespan startup working, addressed sql path issue that was causing table to not be created

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 from sqlmodel import create_engine, SQLModel
 
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:////{sqlite_file_name}"
+#sqlite_file_name = "database.db"
+sqlite_url = "sqlite:///./db.sqlite3"
 
 engine = create_engine(sqlite_url, echo=True)
 
 
 def create_db_and_tables(): 
     SQLModel.metadata.create_all(engine)
+
+
+
+#if __name__ == "__main__":
+    #create_db_and_tables() 
--- main.py
+++ main.py
@@ -2,30 +2,35 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from router.expenses import router as expense_router
 from router.income import router as income_router
-import database
+import database 
+import logging
 
-print("line before api")
-app = FastAPI()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-print("line after api")
+logging.info("line before api")
+
+logging.info("line after api")
 @asynccontextmanager
 async def lifespan(app:FastAPI):
-    await database.create_db_and_tables()
-    print("Database Created")
+    database.create_db_and_tables()
+    logging.info("Database Created")
     #try:
-        #yield
+    yield
     #finally:
         # Cleanup code here, if necessary
         #print("Application shutdown")
 
-@app.router.lifespan_context
-async def lifespan(app):
-    async with lifespan():
-        yield
+app = FastAPI(lifespan=lifespan)
 
-
+@app.get("/")
+def hello():
+    return {"message": "Hello, World"}
 
 
 app.include_router(expense_router, prefix="/expenses")
 app.include_router(income_router, prefix="/income")
 
+if __name__ == "__main__":
+    database.create_db_and_tables()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
