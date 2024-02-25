11c807ebf8bc1839f7ae963fb712a3cbbc050303
Me <davidhyppolitedh@gmail.com>
2024-02-25 13:36:45 -0500

    added session to database testing dummy data for expenses

--- database.py
+++ database.py
@@ -1,12 +1,25 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import Session, create_engine, SQLModel
+from contextlib import contextmanager
 
 
-#sqlite_file_name = "database.db"
+# database URL
 sqlite_url = "sqlite:///./db.sqlite3"
 
+# Creates database engine
 engine = create_engine(sqlite_url, echo=True)
 
+# Session conveyor belt
+@contextmanager
+def get_db():
+    session = Session(engine)
+    try:
+        yield session
+    finally:
+        session.close()
 
+
+
+#Create database and tables
 def create_db_and_tables(): 
     SQLModel.metadata.create_all(engine)
 
--- router/expenses.py
+++ router/expenses.py
@@ -1,9 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models import Expense
-
+from sqlmodel import Session
+from database import get_db
 
 router = APIRouter()
 
 @router.post("/expenses")
-def get_expenses(expense: Expense):
+def get_expenses(expense: Expense, db: Session = Depends(get_db)):
+    dummy_expense = Expense(name="Water", amount=1000, due_date=1)
+    db.add(dummy_expense)
+    db.commit()
+    db.refresh(dummy_expense)
     return {"Message": "Expenses added Successfully" }
+
+@router.get("/expenses")
+def read_expenses(db: Session = Depends(get_db)):
+    expenses = db.query(Expense).all()
+    return expenses
