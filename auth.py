377b6cf844c50d813ac3554e920fb0e71fb55b6b
Me <davidhyppolitedh@gmail.com>
2024-03-05 20:46:50 -0500

    building up JWT authentication

--- auth.py
+++ auth.py
@@ -10,7 +10,7 @@ from router.users import Users
 from sqlmodel import select
 
 
-SECRET_KEY = 
+#SECRET_KEY= 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRES_MINUTES = 30
 
@@ -26,22 +26,48 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     username: str | None = None
 
-
-async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    user = "you"
-    return user
-
-
-def verify_password(plain_password, hased_password):
-    return pwd_context.verify(plain_password, hased_password)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+
 def get_user(db: Session, username: str):
-    statement = select(Users).where(Users,username=username)
+    statement = select(Users).where(Users.username==username)
     result = db.exec(statement).first()
     return result
+
+def authenticate_user(db: Session, username: str, password: str ):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(db: Session = Depends(get_db), data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) +timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, "SECRET_KEY" ,algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    user = decode_token(Token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+
 
 
 
