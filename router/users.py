94de1b15015e9d9014ea31f10b4d6930593813fc
Me <davidhyppolitedh@gmail.com>
2024-03-05 21:28:49 -0500

    editing user endpoints to work with auth

--- router/users.py
+++ router/users.py
@@ -4,14 +4,14 @@ from models import Users, UserIn, UserOut
 from database import get_db, engine
 from datetime import  datetime
 from typing import Annotated
-from auth import oauth2_scheme
+from auth import oauth2_scheme, get_current_user, get_password_hash
 
 router = APIRouter()
 
 
 @router.post("/user", response_model=UserOut)
 def add_user(user: UserIn, db: Session = Depends(get_db)):
-    hashed_password = hashed_password(user.hashed_password)  # Ensure you have a function to hash passwords
+    hashed_password = get_password_hash(user.password)  # Ensure you have a function to hash passwords
     db_user = Users(username=user.username, email=user.email, hashed_password=hashed_password, first_name=user.first_name)
     db.add(db_user)
     db.commit()
@@ -19,8 +19,9 @@ def add_user(user: UserIn, db: Session = Depends(get_db)):
     return db_user
 
 
+
 @router.get("/user/me", response_model=UserOut)
-async def read_user_me(current_user: Annotated[str,Depends(oauth2_scheme)]):
+async def read_user_me(current_user: Annotated[str,Depends(get_current_user)]):
     return current_user
 
 @router.get("/user/{user_id}", response_model=Users)
