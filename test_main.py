c15cb3b1a1d61197952c8d343b2d3b0561be23f4
Me <davidhyppolitedh@gmail.com>
2024-03-09 21:06:57 -0500

    refactored session into pytest fixture

--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import SQLModel, create_engine, Session
 from sqlmodel.pool import StaticPool
@@ -19,15 +20,18 @@ app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
-def test_create_expense():
+@pytest.fixture(name="session")
+def session_fixture():
     engine = create_engine(
         "sqlite://", 
-        connect_args={"check_same_thread": False}, 
-        poolclass=StaticPool,
+        connect_args={"check_same_thread": False}, poolclass=StaticPool,
     )
     SQLModel.metadata.create_all(engine)
     with Session(engine) as session:
+        yield session
 
+
+def test_create_expense(session: Session):
         def get_session_override():
             return session
         
