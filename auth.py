212b15a90944f7a8bf005e9a442d1bce21946584
Me <davidhyppolitedh@gmail.com>
2024-03-11 21:25:52 -0400

    Added secret key

--- auth.py
+++ auth.py
@@ -10,7 +10,7 @@ from router.users import Users
 from sqlmodel import select
 
 
-#SECRET_KEY= 
+SECRET_KEY = "d1fa1da047e93881bfac5d200343e24abef2950559bc868d0d419508a2c2e51f"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRES_MINUTES = 30
 
@@ -53,7 +53,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None ,db: 
     else:
         expire = datetime.now(timezone.utc) +timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, "SECRET_KEY" ,algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY ,algorithm=ALGORITHM)
     return encoded_jwt
 
 async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
@@ -63,7 +63,7 @@ async def get_current_user(db: Session = Depends(get_db), token: str = Depends(o
             headers={"WWW-Authenticate": "Bearer"},
         )
     try:
-        payload = jwt.decode(token, "SECRET_KEY" ,algorithm=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY ,algorithm=ALGORITHM)
         username = payload.get("sub")
         if username is None:
             raise credentials_exception
