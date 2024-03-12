d5ebe6f51623858f8d16cc0d74659cf3e80842be
Me <davidhyppolitedh@gmail.com>
2024-03-12 09:02:50 -0400

    Reducing redundacy in code

--- test_main.py
+++ test_main.py
@@ -8,7 +8,13 @@ from models import Users, Income, Expense
 from auth import authenticate_user, get_user
 from unittest.mock import Mock
 
-
+USER_DATA = {
+        "username": "johndoe",
+        "email": "johndoe@example.com",
+        "first_name": "John",
+        "phone_number": "123-456-7890",
+        "password": "strongpassword"
+    }
 
 @pytest.fixture(name="session")
 def session_fixture():
@@ -26,52 +32,27 @@ def client_fixture(session: Session):
         return session
         
     app.dependency_overrides[get_db] = get_session_override
-
     with TestClient(app) as client:
         yield client
     app.dependency_overrides.clear()
 
 @pytest.fixture(scope="function")
 def create_test_user(client: TestClient):
-    user_data = {
-        "username": "johndoe",
-        "email": "johndoe@example.com",
-        "first_name": "John",
-        "phone_number": "123-456-7890",
-        "password": "strongpassword"
-    }
-    response = client.post("/user", json=user_data)
+    response = client.post("/user", json=USER_DATA)
     assert response.status_code == 201
-    user = response.json()
-    assert 'username' in user, "Key 'username' not in response"
-    assert user["username"] == "johndoe"
-    assert user["email"] == "johndoe@example.com"
-    assert user["first_name"] == "John"
-    assert user["phone_number"] == "123-456-7890"
-    assert user["id"] is not None
+    assert response.json()
+
 
 # Check if user is created
-def test_create_user(client: TestClient):
-    response = client.post(
-        "/user", json={
-        "username": "johndoe",
-        "email": "johndoe@example.com",
-        "first_name": "John",
-        "phone_number": "123-456-7890",
-        "password": "strongpassword"
-        }
-        )
-    data = response.json()
-    print(response.status_code)
-    print(data)
+def test_create_user(client: TestClient, create_test_user):
+    user = create_test_user
 
-    assert response.status_code == 201
-    assert 'username' in data, "Key 'username' not in response"
-    assert data["username"] == "johndoe"
-    assert data["email"] == "johndoe@example.com"
-    assert data["first_name"] == "John"
-    assert data["phone_number"] == "123-456-7890"
-    assert data["id"] is not None
+    assert 'username' in user, "Key 'username' not in response"
+    assert user["username"] == USER_DATA['username']
+    assert user["email"] == USER_DATA['email']
+    assert user["first_name"] == USER_DATA['first_name']
+    assert user["phone_number"] == USER_DATA['phone_number']
+    assert "id" in user
 
 @pytest.fixture(scope="function")
 def test_access_token(client: TestClient, create_test_user):
