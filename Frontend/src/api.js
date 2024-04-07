8ad82adb66ae876fc35cc4b41450b9238b30aaa7
Me <davidhyppolitedh@gmail.com>
2024-04-07 17:30:40 -0400

    resolved merge conflicts

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 
-api.interceptors.request.use((config) => {
+ai.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
         config.headers['Authorization'] = `Bearer ${token}`;
