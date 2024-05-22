de6c43e36677bb186368947c8c9e8776a2d8c901
Me <davidhyppolitedh@gmail.com>
2024-05-22 12:02:48 -0400

    save point

--- Frontend/src/component/Header.jsx
+++ Frontend/src/component/Header.jsx
@@ -1,26 +1,64 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../context/UserContext'
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+import 'bulma/css/bulma.min.css';
 
 const Header = ({ title }) => {
-  // Ensure that you are destructuring the context value correctly
-  // It should match the object structure you provided to UserContext.Provider
-  const { token, setToken } = useContext(UserContext)
+  const { token, setToken } = useContext(UserContext);
 
   const handleLogout = () => {
-    setToken(null)
-    localStorage.removeItem('token') // Also remove the token from localStorage on logout
-  }
+    setToken(null);
+    localStorage.removeItem('token');
+  };
 
   return (
-    <div className='has-text-centered m-6'>
-      <h1 className='title'>{title}</h1>
-      {token && (
-        <button className='button' onClick={handleLogout}>
-          Logout
-        </button>
-      )}
-    </div>
-  )
-}
+    <header>
+      <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            My App
+          </a>
+          <a
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarMenu"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
 
-export default Header
+        <div id="navbarMenu" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item" href="/account-info">
+              Account Information
+            </a>
+            <a className="navbar-item" href="/add-expense">
+              Add Expense
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            {token && (
+              <div className="navbar-item">
+                <button className="button is-light" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container has-text-centered">
+          <h1 className="title">{title}</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
