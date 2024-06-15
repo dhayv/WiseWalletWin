e57562d07c1e779d60cbc8a67882953133642bea
Dhayv <davidhyppolitedh@gmail.com>
2024-06-14 22:16:31 -0400

    finished email client class and add email verification html

--- Backend/Services/email_client.py
+++ Backend/Services/email_client.py
@@ -1,28 +1,37 @@
 from typing import List
-
-from fastapi import BackgroundTasks
+import os
+from dotenv import load_dotenv
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
 from starlette.responses import JSONResponse
-from models import UserIn, Users
+from models import Users
 from sqlmodel import Session, select
 
+load_dotenv()
 
 conf = ConnectionConfig(
-    MAIL_USERNAME ="username",
-    MAIL_PASSWORD = "**********",
-    MAIL_FROM = "test@email.com",
-    MAIL_PORT = 465,
-    MAIL_SERVER = "mail server",
-    MAIL_STARTTLS = False,
-    MAIL_SSL_TLS = True,
-    USE_CREDENTIALS = True,
-    VALIDATE_CERTS = True
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=465,
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
 )
 
+verification_link = os.getenv("SITE_URL")
 
-html = """<p>hey</p>"""
+html = f"""
+    <html>
+    <body>
+        <p>Hello,</p>
+        <p>Click the following link to verify your email:</p>
+        <a href="{verification_link}">{verification_link}</a>
+    </body>
+    </html>
+    """
 
- 
 
 class EmailService:
     def __init__(self, db_session: Session) -> None:
@@ -33,12 +42,15 @@ class EmailService:
         user = self.db.exec(statement).first()
 
         message = MessageSchema(
-            subject="Welcome to WiseWalletWins",
-            recipients=[email]
+            subject="Welcome to WiseWallet",
+            recipients=[user.email],
             body=html,
-            subtype=MessageType.html)
+            subtype=MessageType.html,
+        )
 
         fm = FastMail(conf)
         await fm.send_message(message)
 
-        return JSONResponse(status_code=200, content={"message": "Confirmation email sent successfully"}) 
+        return JSONResponse(
+            status_code=200, content={"message": "Confirmation email sent successfully"}
+        )
