76a792db1c6558f967d06d90cc438c70cc8d6bd8
Dhayv <davidhyppolitedh@gmail.com>
2024-06-17 18:20:21 -0400

    refactor: email client use smtp instead of fastapi mail

--- Backend/Services/email_client.py
+++ Backend/Services/email_client.py
@@ -1,24 +1,14 @@
 import os
-
+import smtplib
+from fastapi import HTTPException
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
-from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
 from models import Users
 from sqlmodel import Session, select
 from starlette.responses import JSONResponse
 
 load_dotenv()
-
-conf = ConnectionConfig(
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=465,
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True,
-)
 
 
 class EmailService:
@@ -32,31 +22,39 @@ class EmailService:
             return JSONResponse(status_code=404, content={"message": "User not found"})
 
         verification_link = f"{os.getenv('SITE_URL')}/verify_email?token={token}"
-        html = f"""
-            <html>
-            <body>
-                <p>Hello,</p>
+        html_content = f"""
+                <p>Hello {user.first_name},</p>
                 <p>Click the following link to verify your email:</p>
                 <a href="{verification_link}">{verification_link}</a>
-            </body>
-            </html>
             """
 
-        message = MessageSchema(
-            subject="Verify your email",
-            recipients=[user.email],
-            body=html,
-            subtype=MessageType.html,
-        )
+        # Email details
+        message = MIMEMultipart()
+        message["From"] = f"WiseWallet {os.getenv('ZOHO_FROM')}"
+        message["To"] = email
+        message["Subject"] = "Verify your email"
+        message.attach(MIMEText(html_content, "html"))
 
-        fm = FastMail(conf)
+        # SMTP configuration
+        smtp_server = os.getenv("ZOHO_SERVER")
+        smtp_port = 2525  # Trying port 2525
+        smtp_username = os.getenv("ZOHO_USERNAME")
+        smtp_password = os.getenv("ZOHO_PASSWORD")
+
         try:
-            await fm.send_message(message)
+            with smtplib.SMTP(smtp_server, smtp_port) as server:
+                print("Connecting to SMTP server")  # Debugging statement
+                server.starttls()
+                server.login(smtp_username, smtp_password)
+                server.sendmail(message["From"], message["To"], message.as_string())
+                print("Email sent successfully")  # Debugging statement
+
             return JSONResponse(
                 status_code=200,
                 content={"message": "Confirmation email sent successfully"},
             )
         except Exception as e:
-            return JSONResponse(
-                status_code=500, content={"message": f"Failed to send email: {str(e)}"}
+            print(f"Failed to send email: {str(e)}")  # Debugging statement
+            raise HTTPException(
+                status_code=500, detail=f"Failed to send email: {str(e)}"
             )
