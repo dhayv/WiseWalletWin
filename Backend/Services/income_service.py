9bd6f790eaff69be69ddc8235f32c67c9432cd37
Dhayv <davidhyppolitedh@gmail.com>
2024-08-16 11:28:28 -0400

    refactor: beanie ODM

--- Backend/Services/income_service.py
+++ Backend/Services/income_service.py
@@ -6,24 +6,21 @@ router = APIRouter()
 
 class IncomeService:
 
-    async def add_income(self, income_data: IncomeBase, user_id: int):
+    async def add_income(self, income_data: IncomeBase, user_id: int) -> Income:
         db_income = await Income(**income_data.model_dump(), user_id=user_id)
 
         await db_income.insert()
         return db_income
 
-    async def read_all_incomes(
-        self,
-        user_id: int,
-    ):
-        incomes = await Income.find(Income.user_id == user_id)
+    async def read_all_incomes(self, user_id: int) -> list[Income]:
+        incomes = await Income.find(Income.user_id == user_id).to_list()
         return incomes
 
     async def update_income(
         self,
         income_id: int,
         income_data: IncomeUpdate,
-    ):
+    ) -> Income:
         db_income = await Income.get(income_id)
         if not db_income:
             raise HTTPException(status_code=404, detail="income not found")
--- Backend/router/income_endpoint.py
+++ Backend/router/income_endpoint.py
@@ -1,38 +1,37 @@
-from data_base.database import get_db
 from fastapi import APIRouter, Depends, status
 from models import Income, IncomeBase, IncomeUpdate
 from Services.income_service import IncomeService
-from sqlmodel import Session
+
 
 router = APIRouter()
 
 
-def get_income_service(db: Session = Depends(get_db)):
-    return IncomeService(db)
+def get_income_service() -> IncomeService:
+    return IncomeService()
 
 
 @router.post(
     "/income/{user_id}", response_model=Income, status_code=status.HTTP_201_CREATED
 )
-def add_income(
+async def add_income(
     income_data: IncomeBase,
     user_id: int,
     service: IncomeService = Depends(get_income_service),
 ):
-    return service.add_income(income_data, user_id)
+    return await service.add_income(income_data, user_id)
 
 
 @router.get("/income/{user_id}", response_model=list[Income])
-def read_all_incomes(
+async def read_all_incomes(
     user_id: int, service: IncomeService = Depends(get_income_service)
 ):
-    return service.read_all_incomes(user_id)
+    return await service.read_all_incomes(user_id)
 
 
 @router.put("/income/{income_id}", response_model=Income)
-def update_income(
+async def update_income(
     income_id: int,
     income_data: IncomeUpdate,
     service: IncomeService = Depends(get_income_service),
 ):
-    return service.update_income(income_id, income_data)
+    return await service.update_income(income_id, income_data)
