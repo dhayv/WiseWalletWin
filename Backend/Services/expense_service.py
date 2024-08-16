b3417bf1927ba8ca865984c2ec548540c189d9a4
Dhayv <davidhyppolitedh@gmail.com>
2024-08-16 10:52:44 -0400

    updated expene service class

--- Backend/Services/expense_service.py
+++ Backend/Services/expense_service.py
@@ -1,10 +1,9 @@
-from fastapi import HTTPException, Response, status
+from fastapi import HTTPException
 from models import Expense, ExpenseBase, ExpenseUpdate, Income
-from beanie import PydanticObjectId
 from typing import Optional
 
-class ExpenseService:
 
+class ExpenseService:
 
     async def add_expense(self, expense_data: ExpenseBase, income_id: int, user_id: int) -> Expense:
 
@@ -16,12 +15,10 @@ class ExpenseService:
         await expense.insert()
         return expense
 
-    async def read_expense(self, income_id: int, user_id: int):
-        statement = select(Expense).where(
+    async def read_expense(self, income_id: int, user_id: int) -> list[Expense]:
+        expenses = await Expense.find(
             Expense.income_id == income_id, Expense.user_id == user_id
         )
-        results = self.db.exec(statement)
-        expenses = results.all()
 
         return expenses
 
@@ -29,21 +26,18 @@ class ExpenseService:
         self,
         expense_id: int,
         expense_data: ExpenseUpdate,
-    ):
-        db_expense = self.db.get(Expense, expense_id)
+    ) -> Optional[Expense]:
+        db_expense = await Expense.get(expense_id)
         if not db_expense:
             raise HTTPException(status_code=404, detail="Expense not found")
-        expense_data_dict = expense_data.model_dump(exclude_unset=True)
-        for key, value in expense_data_dict.items():
-            setattr(db_expense, key, value)
-        self.db.commit()
-        self.db.refresh(db_expense)
+
+        await db_expense.update({"$set": expense_data.model_dump(exclude_unset=True)})
+
         return db_expense
 
-    async def delete_expense(self, expense_id: int):
-        db_expense = self.db.get(Expense, expense_id)
+    async def delete_expense(self, expense_id: int) -> None:
+        db_expense = await Expense.get(expense_id)
         if not db_expense:
             raise HTTPException(status_code=404, detail="Not found")
-        self.db.delete(db_expense)
-        self.db.commit()
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+        await db_expense.delete()
