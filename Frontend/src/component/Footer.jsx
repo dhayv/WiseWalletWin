9a5ca121f05158c7fde9758081a3f70ccc1a1a7f
Dhayv <davidhyppolitedh@gmail.com>
2024-08-17 14:39:24 -0400

    added footer

---
+++ Frontend/src/component/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="content has-text-centered">
+                <p>
+                    <strong>Wise Wallet</strong> by <a href="https://jgthms.com">David Hyppolite</a>.
+                    The source code is licensed
+                    <a href="https://opensource.org/license/mit"> MIT</a>. The
+                    website content is licensed
+                    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"> CC BY NC SA 4.0</a>.
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
