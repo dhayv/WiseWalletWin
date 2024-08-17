089d430da9fee45909d1a2db75c750f3a351923e
Dhayv <davidhyppolitedh@gmail.com>
2024-08-17 14:28:16 -0400

    connected react router changed main layout

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { UserProvider } from './context/UserContext.jsx'
+import * as React from 'react';
+import {createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { UserProvider } from './context/UserContext.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <UserProvider>
-      <App />
+      <RouterProvider router={router} />
     </UserProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
