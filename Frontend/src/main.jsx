fb9a2406dcd77f33880587d9253447a7960cda9a
Dhayv <davidhyppolitedh@gmail.com>
2024-08-17 14:51:04 -0400

    added basic paths

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,61 @@ import * as React from 'react';
 import {createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { UserProvider } from './context/UserContext.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Header from './component/Header.jsx';
+import Footer from './component/Footer.jsx';
+import ErrorMessage from './component/ErrorMessage.jsx';
+import Login from './component/Login';
+import SignUp from './component/SignUp';
+import Income from './component/Income';
+import Remaining from './component/Remaining';
+import NextCheck from './component/NextCheck';
+import Expenses from './component/Expenses';
 
+
+export const Layout = () => {
+  return (
+    <div id="page-content">
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>,
+    errorElement:<ErrorMessage/>,
+    element: <Layout/> ,
+    children: [
+      {
+        path: '/',
+        element: <App/>,
+      },
+      {
+        path: '/login',
+        element: <Login/>,
+      },
+      {
+        path: '/signup',
+        element: <SignUp/>,
+      },
+      {
+        path: '/income',
+        element: <Income/>,
+      },
+      {
+        path: '/expense',
+        element: <Expenses/>,
+      },
+      {
+        path: '/remaining',
+        element: <Remaining/>,
+      },
+      {
+          path: '/next-check',
+          element: <NextCheck/>,
+      }
+    ]
   }
 ])
 
