1cd4e85fa09dafa93f229ad9830c394d42826dfb
Hayvid <davidhyppolitedh@gmail.com>
2024-09-09 08:20:43 -0400

    refactor: cleaned log

--- Backend/data_base/database.py
+++ Backend/data_base/database.py
@@ -16,7 +16,6 @@ logging.basicConfig(
 async def init_db(models: list[Document]):
     try:
         await init_beanie(database=get_database, document_models=models)
-        logging.info("Successfully initialized beanie with MongoDB!")
 
     except Exception as e:
         logging.error(f"An error occurred while initializing beanie: {e}")
