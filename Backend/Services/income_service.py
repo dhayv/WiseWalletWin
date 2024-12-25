a9c29c53fe44613be3006a1def618cc6cf907a30
Hayvid <davidhyppolitedh@gmail.com>
2024-12-25 14:44:48 -0500

    update

--- Backend/Services/income_service.py
+++ Backend/Services/income_service.py
@@ -8,21 +8,29 @@ router = APIRouter()
 class IncomeService:
 
     async def add_income(self, income_data: IncomeBase, user_id: str) -> Income:
-        if income_data is None:
-            raise HTTPException(status_code=400, detail="Null not allowed")
-
         existing_income = await Income.find_one(
             {"user_id.$id": PydanticObjectId(user_id)}
         )
+        if income_data.amount is None:
+            income_data.amount = 0.0
 
         if existing_income:
+
             # Update the existing income entry
             update_data = income_data.model_dump(exclude_unset=True)
             await existing_income.update({"$set": update_data})
             return existing_income
         else:
-            # Create a new income entry
-            new_income = Income(**income_data.model_dump(), user_id=user_id)
+            # Create a new income entry defalut amount value is 0
+            default_amount = income_data.amount if income_data.amount is not None else 0
+
+            new_income = Income(
+                amount=default_amount,
+                recent_pay=income_data.recent_pay,
+                last_pay=income_data.last_pay,
+                user_id=user_id
+            )
+
             await new_income.insert()
             return new_income
 
@@ -30,7 +38,6 @@ class IncomeService:
         incomes = await Income.find(
             {"user_id.$id": PydanticObjectId(user_id)}
         ).to_list()
-
         return incomes
 
     async def update_income(
