251641c085bdeb6b2df15999a49b923b7867c93c
Hayvid <davidhyppolitedh@gmail.com>
2025-06-01 13:34:07 -0400

    update to remove root path, added models

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from contextlib import asynccontextmanager
@@ -5,11 +6,11 @@ from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from models import Expense, Income, Users
 from mangum import Mangum
 
 from data_base.database import init_db
 from data_base.db_utils import check_connection
+from models import Expense, Income, Users
 from router import expenses_endpoint as expense_router
 from router import income_endpoint as income_router
 from router import user_endpoint as user_router
@@ -32,7 +33,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, debug=True)
 
-origins = os.getenv("ORIGIN_URL")
+origins = json.loads(os.getenv("ORIGIN_URL", "[]"))
 
 
 app.add_middleware(
@@ -44,6 +45,15 @@ app.add_middleware(
 )
 
 
+@app.middleware("http")
+async def log_request_path(request, call_next):
+    import logging
+
+    logging.info(f"📥 Incoming path: {request.url.path}")
+    response = await call_next(request)
+    return response
+
+
 @app.get("/")
 def hello():
     return {"message": "Hello, World"}
@@ -53,7 +63,7 @@ app.include_router(expense_router.router, prefix="/api")
 app.include_router(income_router.router, prefix="/api")
 app.include_router(user_router.router, prefix="/api")
 
-handler = Mangum(app, lifespan="off", api_gateway_base_path="/default")
+handler = Mangum(app, lifespan="off")
 
 if __name__ == "__main__":
     import uvicorn
