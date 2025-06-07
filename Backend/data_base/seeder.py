6034293687d758d359ef38e5cef75a9dcd50017e
Hayvid <davidhyppolitedh@gmail.com>
2025-06-07 14:02:24 -0400

    simplified seed code

--- Backend/data_base/seeder.py
+++ Backend/data_base/seeder.py
@@ -2,13 +2,7 @@ from constants import default_categories
 from models import Category
 
 
-async def prepopulate_categories():
-
+async def prepopulate_categories() -> None:
     for cat in default_categories:
-        existing = await Category.find_one(Category.name == cat["name"])
-        if not existing:
-            new_cat = Category(**cat)
-            await new_cat.insert()
-            print(f"Inserted category: {new_cat.name}")
-        else:
-            print(f"Category {cat['name']} already exists.")
+        if not await Category.find_one(Category.name == cat["name"]):
+            await Category(**cat).insert()
