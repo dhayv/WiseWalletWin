4352c15e892e3a802b1d573f4b4a5c6a1ea4faf0
Hayvid <davidhyppolitedh@gmail.com>
2025-07-04 15:34:01 -0400

    update to tsx, null protection for create root, title for Header

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,21 +3,22 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App'
+import './App.jsx'
 import { UserProvider } from './context/UserContext.jsx'
 import './input.css'
 
-import Header from './component/Header.jsx'
+import Header from './component/Header'
 import Footer from './component/Footer.jsx'
 import Homepage from './component/HomePage.jsx'
 import ErrorMessage from './component/ErrorMessage.jsx'
-import Login from './component/Login'
-import SignUp from './component/SignUp'
-import ProtectedRoute from './component/ProtectedRoute'
+import Login from './component/Login.jsx'
+import SignUp from './component/SignUp.jsx'
+import ProtectedRoute from './component/ProtectedRoute.jsx'
 import Income from './component/Income.jsx'
 import Expense from './component/Expenses.jsx'
-import Remaining from './component/Remaining'
-import NextCheck from './component/NextCheck'
+import Remaining from './component/Remaining.jsx'
+import NextCheck from './component/NextCheck.jsx'
+
 
 if (import.meta.env.PROD) {
   console.log = function () {}
@@ -26,7 +27,7 @@ if (import.meta.env.PROD) {
 export const Layout = () => (
   <div id='page-content'>
     <ProtectedRoute>
-      <Header />
+      <Header title={'string'} />
     </ProtectedRoute>
     <Outlet />
     <ProtectedRoute>
@@ -103,7 +104,10 @@ const router = createBrowserRouter([
   }
 ])
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root')
+if (!root) {throw new Error("Root is missing")};
+
+createRoot(root).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
